a446f21af7b9235420e2ef29cb4f2cba5013171b
agatatalita <agatamonikaandrzejewska@gmail.com>
2020-06-02 09:50:31 +0200

    Updates

---
+++ precache-manifest.8cf27045d5dc22db0517110aba968fa5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c734eeea1a47338dfa0a1c1159f9a948",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "0b8f8ce2a79df8830c35",
+    "url": "/react-gh-pages/static/css/main.6a9d1ba3.chunk.css"
+  },
+  {
+    "revision": "b7da7ca33ada40a1eb61",
+    "url": "/react-gh-pages/static/js/2.be50b970.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-gh-pages/static/js/2.be50b970.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b8f8ce2a79df8830c35",
+    "url": "/react-gh-pages/static/js/main.951f0ccf.chunk.js"
+  },
+  {
+    "revision": "9a7a8fb07ac25676ce95",
+    "url": "/react-gh-pages/static/js/runtime-main.9aad8695.js"
+  }
+]);
