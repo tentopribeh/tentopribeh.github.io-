2d12dc124640d0d15e7644febf7b2b465806ab70
agatatalita <agatamonikaandrzejewska@gmail.com>
2020-06-02 10:01:45 +0200

    Updates

---
+++ precache-manifest.8d75f53900fa3b39645a37bf9a3ccde1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cabf8c4dfa7c18cc262e4f4f6539bb4c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0b8f8ce2a79df8830c35",
+    "url": "/static/css/main.6a9d1ba3.chunk.css"
+  },
+  {
+    "revision": "b7da7ca33ada40a1eb61",
+    "url": "/static/js/2.be50b970.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.be50b970.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b8f8ce2a79df8830c35",
+    "url": "/static/js/main.951f0ccf.chunk.js"
+  },
+  {
+    "revision": "99fc0b5bcfe99df2e985",
+    "url": "/static/js/runtime-main.6d7c4f51.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-gh-pages/precache-manifest.8cf27045d5dc22db0517110aba968fa5.js"
+  "/precache-manifest.8d75f53900fa3b39645a37bf9a3ccde1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-gh-pages/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
